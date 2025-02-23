8fcea2b823471d03a8a6313d0f179357f162202b
bvenkataswamy <venkataswamyb07@gmail.com>
2025-02-22 20:40:38 -0600

    Connection to the mysql database with single entity

---
+++ src/main/java/com/venkat/MultipleDatabaseDemoApplication.java
@@ -0,0 +1,27 @@
+package com.venkat;
+
+import com.venkat.productentity.Product;
+import com.venkat.productrepository.ProductDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleDatabaseDemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private ProductDAO productDAO;
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleDatabaseDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Product product = new Product(1,"laptop",25000.00);
+		productDAO.save(product);
+
+
+	}
+}
