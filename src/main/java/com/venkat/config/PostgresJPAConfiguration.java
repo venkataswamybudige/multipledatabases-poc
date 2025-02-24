9fb5880dee901b52b35ded2fbbf36614cfd40434
bvenkataswamy <venkataswamyb07@gmail.com>
2025-02-23 20:04:00 -0600

    code cleanup

--- src/main/java/com/venkat/config/PostgresJPAConfiguration.java
+++ src/main/java/com/venkat/config/PostgresJPAConfiguration.java
@@ -1,46 +1,5 @@
-//package com.venkat.config;
-//
-//import jakarta.persistence.EntityManagerFactory;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-//import org.springframework.orm.jpa.JpaTransactionManager;
-//import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-//import org.springframework.transaction.PlatformTransactionManager;
-//import org.springframework.transaction.annotation.EnableTransactionManagement;
-//
-//import javax.sql.DataSource;
-//
-//@Configuration
-//@EnableTransactionManagement
-//@EnableJpaRepositories(
-//        basePackages = "com.venkat.orderrepository",
-//        entityManagerFactoryRef = "postgresEntityManagerFactoryBean",
-//        transactionManagerRef = "postgresTransactionManager"
-//)
-//public class PostgresJPAConfiguration {
-//
-//    @Bean
-//    LocalContainerEntityManagerFactoryBean postgresEntityManagerFactoryBean(EntityManagerFactoryBuilder entityManagerFactoryBuilder,@Qualifier("postgresDataSource") DataSource dataSource){
-//        return  entityManagerFactoryBuilder.
-//                dataSource(dataSource).
-//                packages("com.venkat.orderentity").
-//                build();
-//
-//    }
-//
-//    @Bean
-//    public JpaTransactionManager postgresTransactionManager(@Qualifier("postgresEntityManagerFactoryBean") EntityManagerFactory postgresEntityManagerFactoryBean) {
-//        return new JpaTransactionManager(postgresEntityManagerFactoryBean);
-//    }
-//}
-
-
 package com.venkat.config;
 
-import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
